aa76f2a9d83d297972a1f0849519af7f913f5a94
poltergeist42 <lepoltergeist@gmail.com>
2016-06-08 20:11:06 +0200

    creation d'un binnaire a l'aide de "py2exe" (setup.py)

--- 03_software/batteryStatus.py
+++ 03_software/batteryStatus.py
@@ -16,12 +16,16 @@ class SYSTEM_POWER_STATUS(ctypes.Structure):
         ('BatteryFullLifeTime', wintypes.DWORD),
     ]
 
+# def f_statusInit():
 SYSTEM_POWER_STATUS_P = ctypes.POINTER(SYSTEM_POWER_STATUS)
-
 GetSystemPowerStatus = ctypes.windll.kernel32.GetSystemPowerStatus
 GetSystemPowerStatus.argtypes = [SYSTEM_POWER_STATUS_P]
 GetSystemPowerStatus.restype = wintypes.BOOL
 status = SYSTEM_POWER_STATUS()
+if not GetSystemPowerStatus(ctypes.pointer(status)):
+    raise ctypes.WinError()
+
+
 
 def f_printBF() :
     """
@@ -30,7 +34,7 @@ def f_printBF() :
     """
     system("cls")
     print("\n\n")
-    print("\t######                                         ")                                
+    print("\t######                                         ")
     print("\t#     #   ##   ##### ##### ###### #####  #   # ")
     print("\t#     #  #  #    #     #   #      #    #  # #  ")
     print("\t######  #    #   #     #   #####  #    #   #   ")
@@ -47,8 +51,8 @@ def f_printBF() :
     print("\t         #        ####  ###### ######          ")
     print("\n\n")
 
-    if not GetSystemPowerStatus(ctypes.pointer(status)):
-        raise ctypes.WinError()
+    # if not GetSystemPowerStatus(ctypes.pointer(status)):
+        # raise ctypes.WinError()
     print ('ACLineStatus', status.ACLineStatus)
     print ('BatteryFlag', status.BatteryFlag)
     print ('BatteryLifePercent', status.BatteryLifePercent, " - type : ", type(status.BatteryLifePercent))
@@ -56,10 +60,14 @@ def f_printBF() :
     print ('BatteryFullLifeTime', status.BatteryFullLifeTime)
 
 def main() :
-
-    if status.BatteryLifePercent == 100 :
+    print("dbgMsg : status.BatteryLifePercent : ",status.BatteryLifePercent)
+    # test = status.BatteryLifePercent
+    # print(test, " - ", type(test))
+    if (status.ACLineStatus) and (status.BatteryLifePercent == 100) :
         f_printBF()
-        input("appuyer sur entree pour fermer la fenetre")
+        # input("\nappuyer sur entree pour fermer la fenetre")
+        
+    input("\nappuyer sur entree pour fermer la fenetre")
 
 
 if __name__ == '__main__':
