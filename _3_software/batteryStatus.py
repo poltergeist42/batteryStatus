6b89a0890f4ab7166cfe7fb9f2ccbabd37a6ead2
poltergeist42 <lepoltergeist@gmail.com>
2016-12-07 14:03:44 +0100

    WIP : ajout de la class C_BatteryStatus

--- _3_software/batteryStatus.py
+++ _3_software/batteryStatus.py
@@ -4,7 +4,7 @@
 """
    :Nom du fichier:     batteryStatus.py
    :Autheur:            `Poltergeist42 <https://github.com/poltergeist42>`_
-   :Version:            20161206
+   :Version:            20161207
 
 ----
 
@@ -47,57 +47,84 @@ for i, b in enumerate(batts1):
     print( 'Battery %d Design Capacity: %d mWh' % (i, b.DesignCapacity or 0) )
 
 
+class C_BatteryStatus( object ) :
+    """
+    """
+    def __ini__( self, v_levelChk = 100 ) :
+        """ initialisation des varraibles """
+        self.v_levelChk                 = v_levelChk
+        self.v_FullChargedCapacity      = False
+        self.InstanceName               = False
+        self.PowerOnline                = False
+        self.Charging                   = False
+        self.DischargeRate              = False
+        self.ChargeRate                 = False
+        self.RemainingCapacity          = False
+        self.ChargeLvel                 = False
+        
 
+    def f_setBatteryValue( self ) :
+        """ Permet d'initialiser les variables de la batterie """
+        batts = t.ExecQuery('Select * from BatteryFullChargedCapacity')
+                    # la methode : ExecQuery fait des requette SQL pour intéroger la base WMI
+        for i, b in enumerate(batts):
+            self.v_FullChargedCapacity = b.FullChargedCapacity
 
-def f_batInfo () :
+        batts = t.ExecQuery('Select * from BatteryStatus where Voltage > 0')
 
-    v_batFull = False
-    batts = t.ExecQuery('Select * from BatteryFullChargedCapacity')
-    for i, b in enumerate(batts):
-        v_batFull = b.FullChargedCapacity
-        print( "Charged Capacity: {} mWh".format(v_batFull))
+        for i, b in enumerate(batts):
 
-    batts = t.ExecQuery('Select * from BatteryStatus where Voltage > 0')
-
-    for i, b in enumerate(batts):
+            self.InstanceName       = b.InstanceName
+            self.PowerOnline        = b.PowerOnline
+            self.Charging           = b.Charging
+            self.DischargeRate      = b.DischargeRate
+            self.ChargeRate         = b.ChargeRate
+            self.RemainingCapacity  = b.RemainingCapacity
+            self.ChargeLvel         = round(((b.RemainingCapacity*100) /self.v_FullChargedCapacity), 2 )
+        
+    def f_printBatteryValue( self ) :
+        """ Affiche les données de la batterie """
+        print( "Name:               \t{}".format(self.InstanceName ))
+        print( "PowerOnline:        \t{}".format(self.PowerOnline) )
+        print( "Charging:           \t{}".format(self.Charging))
+        print( "DischargeRate:      \t{}".format(self.DischargeRate))
+        print( "ChargeRate:         \t{}".format(self.ChargeRate))
+        print( "Charged Capacity:   \t{} mWh".format(self.v_FullChargedCapacity))
+        print( "RemainingCapacity:  \t{}".format(self.RemainingCapacity))
+        print( "ChargeLvel (%)      \t{}%".format(self.ChargeLvel))
         # print( '\nBattery %d ***************' % i )
         # print( 'Tag:               ' + str(b.Tag) )
-        print( "Name:               \t{}".format(b.InstanceName ))
-
-        print( "PowerOnline:        \t{}".format(b.PowerOnline) )
         # print( 'Discharging:       \t', b.Discharging)
-        print( "Charging:           \t{}".format(b.Charging))
         # print( 'Voltage:           \t', b.Voltage)
-        print( "DischargeRate:      \t{}".format(b.DischargeRate))
-        print( "ChargeRate:         \t{}".format(b.ChargeRate))
-        print( "RemainingCapacity:  \t{}".format(b.RemainingCapacity))
         # print( 'Active:            \t', b.Active)
         # print( 'Critical:          \t', b.Critical)
-        print( "ChargeLvel (%)      \t{}%".format(round(((b.RemainingCapacity*100) /v_batFull), 2 )))
+        
+    def f_levelChk( self ) :
+        """ Test si le taux de charge et supprérieur ou égal à la valeur de v_levelChk """
 
-# def f_printBF() :
-    # """
-    # banniere realise depuis le site :
-    # http://www.network-science.de/ascii/
-    # """
-    # system("cls")
-    # print("\n\n")
-    # print("\t######                                         ")
-    # print("\t#     #   ##   ##### ##### ###### #####  #   # ")
-    # print("\t#     #  #  #    #     #   #      #    #  # #  ")
-    # print("\t######  #    #   #     #   #####  #    #   #   ")
-    # print("\t#     # ######   #     #   #      #####    #   ")
-    # print("\t#     # #    #   #     #   #      #   #    #   ")
-    # print("\t######  #    #   #     #   ###### #    #   #   ")
-    # print("\t                                               ")
-    # print("\t         #######                               ")
-    # print("\t         #       #    # #      #               ")
-    # print("\t         #       #    # #      #               ")
-    # print("\t         #####   #    # #      #               ")
-    # print("\t         #       #    # #      #               ")
-    # print("\t         #       #    # #      #               ")
-    # print("\t         #        ####  ###### ######          ")
-    # print("\n\n")
+def f_printBF() :
+    """
+    banniere realise depuis le site :
+    http://www.network-science.de/ascii/
+    """
+    system("cls")
+    print("\n\n")
+    print("\t######                                         ")
+    print("\t#     #   ##   ##### ##### ###### #####  #   # ")
+    print("\t#     #  #  #    #     #   #      #    #  # #  ")
+    print("\t######  #    #   #     #   #####  #    #   #   ")
+    print("\t#     # ######   #     #   #      #####    #   ")
+    print("\t#     # #    #   #     #   #      #   #    #   ")
+    print("\t######  #    #   #     #   ###### #    #   #   ")
+    print("\t                                               ")
+    print("\t         #######                               ")
+    print("\t         #       #    # #      #               ")
+    print("\t         #       #    # #      #               ")
+    print("\t         #####   #    # #      #               ")
+    print("\t         #       #    # #      #               ")
+    print("\t         #       #    # #      #               ")
+    print("\t         #        ####  ###### ######          ")
+    print("\n\n")
 
 
 
